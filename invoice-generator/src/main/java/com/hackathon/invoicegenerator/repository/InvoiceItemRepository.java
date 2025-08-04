12498f7a5a380ca63155db6db2af70c13d324888
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-08-04 11:21:21 +0530

    Removed previous invoice-generator directory

--- invoice-generator/src/main/java/com/hackathon/invoicegenerator/repository/InvoiceItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hackathon.invoicegenerator.repository;
-
-import com.hackathon.invoicegenerator.entity.InvoiceItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvoiceItemRepository extends JpaRepository<InvoiceItem, Long> {
-}
--- invoice-generator/src/main/java/com/hackathon/invoicegenerator/repository/InvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hackathon.invoicegenerator.repository;
-
-import com.hackathon.invoicegenerator.entity.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-}
