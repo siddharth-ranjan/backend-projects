189a1aa4a1a2b3ced80d753e0659e99e80f9c765
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-08-04 16:01:45 +0530

    chore(init): initialize project structure with basic files

---
+++ broadcast-server/src/main/java/com/hackathon/broadcastserver/BroadcastServerApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.broadcastserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroadcastServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroadcastServerApplication.class, args);
+    }
+
+}
---
+++ broadcast-server/src/test/java/com/hackathon/broadcastserver/BroadcastServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.broadcastserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroadcastServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
