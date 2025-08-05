2fd0e3d30ef95e3cc5c633648b4c21a1c738c557
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-08-05 09:58:37 +0530

    chore(init): initial project structure

---
+++ task-tracker-cli/src/main/java/com/hackathon/tasktrackercli/TaskTrackerCliApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.tasktrackercli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerCliApplication.class, args);
+    }
+
+}
---
+++ task-tracker-cli/src/test/java/com/hackathon/tasktrackercli/TaskTrackerCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.tasktrackercli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
