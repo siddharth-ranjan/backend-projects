3b1607c326434c25687ddd20fb38d0c79a0d8f0b
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-08-07 00:11:25 +0530

    chore: removed unused imports

--- github-user-activity/src/main/java/com/hackathon/githubuseractivity/record/GithubEvent.java
+++ github-user-activity/src/main/java/com/hackathon/githubuseractivity/record/GithubEvent.java
@@ -3,7 +3,6 @@ package com.hackathon.githubuseractivity.record;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.List;
 import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
--- github-user-activity/src/main/java/com/hackathon/githubuseractivity/service/GithubService.java
+++ github-user-activity/src/main/java/com/hackathon/githubuseractivity/service/GithubService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Service
 public class GithubService {
