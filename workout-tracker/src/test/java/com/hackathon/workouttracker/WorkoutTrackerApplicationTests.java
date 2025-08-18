8c2d3fc512f1c1b04ff7d828ee106d575fae80d8
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-08-18 16:14:39 +0530

    chore(init): basic project structure

---
+++ workout-tracker/src/test/java/com/hackathon/workouttracker/WorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.workouttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
