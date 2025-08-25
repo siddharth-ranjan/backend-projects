8f566a2b1cde9d1d321cd333f1b4d02f31c57a90
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2025-08-25 23:57:01 +0530

    chore(init): basic project structure

---
+++ real-time-learderboard/src/main/java/com/hackathon/realtimelearderboard/RealTimeLearderboardApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.realtimelearderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeLearderboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeLearderboardApplication.class, args);
+    }
+
+}
---
+++ real-time-learderboard/src/test/java/com/hackathon/realtimelearderboard/RealTimeLearderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.realtimelearderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeLearderboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
